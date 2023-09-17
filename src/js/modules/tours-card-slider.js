64629fbc685071cd1cc0eb99df983774babf4003
volches <peala@mail.ru>
2023-09-17 23:19:57 +0300

    Form

--- src/js/modules/tours-card-slider.js
+++ src/js/modules/tours-card-slider.js
@@ -1,45 +1,88 @@
 import Swiper from 'swiper/bundle';
 import 'swiper/css/bundle';
 
-function swiperFunc () {
-	const swiper = new Swiper('.tours-card-slider', {
-		slidesPerView: 1,
-		spaceBetween: 45,
-		grabCursor: true,
-		// mousewheel:true,
-		loop: true,
-		a11y: false,
-		freeMode: true,
-		speed: 4000,
-		pagination: {
-			el: ".swiper-pagination",
-			clickable: true
-		},
-		autoplay: {
-			delay: 0.01,
-			disableOnInteraction: true,
-		},
-		breakpoints: {
-			320: {
-				slidesPerView: 1
-			},
-			1440: {
-				slidesPerView: 2,
-				direction: 'vertical',
+let swiper;
+
+// function swiperFunc () {
+	// const swiper = new Swiper('.tours-card-slider', {
+	// 	slidesPerView: 1,
+	// 	spaceBetween: 45,
+	// 	grabCursor: true,
+	// 	// mousewheel:true,
+	// 	loop: true,
+	// 	freeMode: true,
+	// 	pagination: {
+	// 		el: ".swiper-pagination",
+	// 		clickable: true
+	// 	},
+		// breakpoints: {
+		// 	320: {
+		// 		slidesPerView: 1
+		// 	},
+		// 	1440: {
+		// 		slidesPerView: 2,
+		// 		direction: 'vertical',
+		// 		a11y: false,
+		// 		speed: 3000,
+		// 		autoplay: true,
+		// 		autoplay: {
+		// 			delay: 0.01,
+		// 			disableOnInteraction: true,
+		// 		},
 				
-			}
-		}
-	});
-}
-
-// Устанавливаем брейкпоинт
-// const breakpoint = window.matchMedia('(max-width:1439px)');
-
-// Ф-я которая проверяет срабатываение медиа запроса в breakpoint
-// const breakpointChecker = function (){
-// 	if (breakpoint.matches === true){
-// 		swiper
-// 	}
+		// 	}
+		// }
+// 	});
 // }
 
-export default swiperFunc;
+const configHorizontal = {
+	direction: 'horizontal',
+	slidesPerView: 1,
+	spaceBetween: 45,
+	grabCursor: true,
+	freeMode: true,
+	speed: 2000,
+	loop: true,
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true
+	},
+	// autoplay: {
+	// 	delay: 0.0,
+	// 	disableOnInteraction: false,
+	// },
+};
+
+const configVertical = {
+	direction: 'vertical',
+	slidesPerView: 2,
+	spaceBetween: 0,
+	grabCursor: true,
+	a11y: false,
+	freeMode: true,
+	speed: 2000,
+	loop: true,
+	autoplay: {
+		delay: 0.0,
+		disableOnInteraction: false,
+	},
+};
+
+// Устанавливаем брейкпоинт
+const breakpoint = window.matchMedia('(max-width:1439px)');
+
+// Ф-я которая проверяет срабатываение медиа запроса в breakpoint
+const breakpointChecker = function (){
+	if (breakpoint.matches === true){
+		swiper = new Swiper('.tours-card-slider', configHorizontal)
+	} else if (breakpoint.matches === false) {
+		swiper = new Swiper('.tours-card-slider', configVertical)
+	}
+};
+
+breakpoint.addListener(breakpointChecker);
+breakpointChecker();
+
+// export default swiperFunc;
+
+export default breakpointChecker;
