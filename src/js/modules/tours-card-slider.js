c46e52025577bea17feed98a184c5ebbe9def10f
volches <peala@mail.ru>
2023-09-17 18:25:52 +0300

    Slider-cards

--- src/js/modules/tours-card-slider.js
+++ src/js/modules/tours-card-slider.js
@@ -3,15 +3,18 @@ import 'swiper/css/bundle';
 
 function swiperFunc () {
 	const swiper = new Swiper('.tours-card-slider', {
-		direction: 'vertical',
 		slidesPerView: 1,
-		spaceBetween: 0,
+		spaceBetween: 45,
 		grabCursor: true,
 		// mousewheel:true,
 		loop: true,
 		a11y: false,
 		freeMode: true,
-		speed: 3000,
+		speed: 4000,
+		pagination: {
+			el: ".swiper-pagination",
+			clickable: true
+		},
 		autoplay: {
 			delay: 0.01,
 			disableOnInteraction: true,
@@ -20,11 +23,23 @@ function swiperFunc () {
 			320: {
 				slidesPerView: 1
 			},
-			833: {
+			1440: {
 				slidesPerView: 2,
+				direction: 'vertical',
+				
 			}
 		}
 	});
 }
+
+// Устанавливаем брейкпоинт
+// const breakpoint = window.matchMedia('(max-width:1439px)');
+
+// Ф-я которая проверяет срабатываение медиа запроса в breakpoint
+// const breakpointChecker = function (){
+// 	if (breakpoint.matches === true){
+// 		swiper
+// 	}
+// }
 
 export default swiperFunc;
